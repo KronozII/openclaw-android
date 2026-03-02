27b9bb43f8c73cab8409d98ce9d6bfc82e89ef39
KronozII <code.cryptical@gmail.com>
2026-03-02 00:53:35 -0600

    mod gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     repositories {
-        google { content { includeGroupByRegex("com\\.android.*"); includeGroupByRegex("com\\.google.*"); includeGroupByRegex("androidx.*") } }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -11,11 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-    }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://www.zetetic.net/maven") }
     }
 }
 
